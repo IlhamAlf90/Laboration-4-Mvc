bf43545d3c976cde4ec062715ce49b5c2c431612
Ilham Alfayyad <147632236+IlhamAlf90@users.noreply.github.com>
2025-04-06 19:56:33 +0200

    Update Categories.cs (string to byte) and (?)

--- Models/Categories.cs
+++ Models/Categories.cs
@@ -4,8 +4,8 @@
     {
         public int CategoryID { get; set; }
         public string CategoryName { get; set; }
-        public string Description { get; set; }
-        public string Picture { get; set; }
+        public string? Description { get; set; }
+        public byte[]? Picture { get; set; }
 
     }
 }
